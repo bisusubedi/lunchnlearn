79d29f22d74df7af2a4130a0f39a2dbd13c93a82
Bishnu Subedi <bisusubedi@gmail.com>
2012-12-03 22:34:15 +0545

    validation and relation added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,13 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
+  validates :full_name ,:presence => true 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+ 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me,:full_name
   # attr_accessible :title, :body
+  
+  has_many :topics
 end
