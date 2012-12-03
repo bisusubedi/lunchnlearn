095e392d2e476813f5293c1d6d2ee4728e9c9d09
Bishnu Subedi <bisusubedi@gmail.com>
2012-12-03 22:33:54 +0545

    routes for topics and root path added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Lunchnlearn::Application.routes.draw do
+  resources :topics
+
+  devise_for :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +53,7 @@ Lunchnlearn::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => "topics#index"
 
   # See how all your routes lay out with "rake routes"
 
