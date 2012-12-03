cbc22aa13563ca03a5c58524b6250e1ad54e7b2a
Bishnu Subedi <bisusubedi@gmail.com>
2012-12-03 17:57:15 +0545

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lunchnlearn::Application.config.secret_token = '0795ddbff21c2eb668da35708b0871133fca69e1dedfdd03695cce7f09bf18a7cc23b1a2f99723b6ec645dcd572db89100b1650426c2cbfb28cf0f16d53c8636'
